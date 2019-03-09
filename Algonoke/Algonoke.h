dc0ce2d7354960bf3dea9dc25a2a2c22ca3dc0f9
totoro97 <totoro97@outlook.com>
2019-03-09 14:57:00 +0800

    sync

--- Algonoke/Algonoke.h
+++ Algonoke/Algonoke.h
@@ -3,6 +3,7 @@
 
 namespace Algo {
 
+// Mincost of matching!
 void MatchingKM(int size, double *bi_cost, std::vector<int> &pas);
 
 }
--- Algonoke/TestAll.cpp
+++ Algonoke/TestAll.cpp
@@ -7,10 +7,16 @@ void TestKM() {
   int n = 4;
   double *cost = new double[n * n];
   for (int i = 0; i < n * n; i++) {
-    cost[i] = 1.0;
+    cost[i] = 0.0;
+  }
+  for (int i = 0; i < n; i++) {
+    cost[i * n + i] = -1.0;
   }
   std::vector<int> pas;
   MatchingKM(n, cost, pas);
+  for (int i = 0; i < n; i++)
+    std::cout << pas[i] << " ";
+  std::cout << std::endl;
   delete[](cost);
 }
 
