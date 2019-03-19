e428cee0940e39a71f7d370378cd1fab05b0448b
totoro97 <totoro97@outlook.com>
2019-03-19 15:05:49 +0800

    add sparse matching

--- Algonoke/Algonoke.cpp
+++ Algonoke/Algonoke.cpp
@@ -69,4 +69,73 @@ void MatchingKM(int size, double *bi_cost, std::vector<int> &pas) {
   }
 }
 
+// Sparse graph.
+
+bool SparseHavePathKM(int i, int n, std::vector<std::pair<int, double> > *edges,
+                std::vector<double> &slk, std::vector<int> &vis_l,
+                std::vector<int> &vis_r, std::vector<double> &mark_l, std::vector<double> &mark_r,
+                std::vector<int> &p) {
+  vis_l[i] = 1;
+  for (const auto &edge : edges[i]) {
+    int j = edge.first;
+    double cost = edge.second;
+    if (std::abs(cost - mark_l[i] - mark_r[j]) < 1e-3) {
+      if (vis_r[j])
+        continue;
+      vis_r[j] = 1;
+      if (p[j] == -1 || SparseHavePathKM(p[j], n, edges, slk, vis_l, vis_r, mark_l, mark_r, p)) {
+        p[j] = i;
+        // std::cout << "Yes " << i << " " << j << std::endl;
+        return true;
+      }
+    }
+    else {
+      slk[j] = std::min(slk[j], cost - mark_l[i] - mark_r[j]);
+    }
+  }
+  return false;
+}
+
+void SparseMatchingKM(int size, std::vector<std::pair<int, double> > *edges, std::vector<int> &pas) {
+  int n = size;
+  pas.resize(n, -1);
+  std::vector<double> mark_l(n, 1e9);
+  std::vector<double> mark_r(n, 0.0);
+
+  for (int i = 0; i < n; i++) {
+    for (const auto &edge : edges[i]) {
+      mark_l[i] = std::min(mark_l[i], edge.second);
+    }
+  }
+
+  std::vector<double> slk(n, 0.0);
+  std::vector<int> vis_l(n, 0);
+  std::vector<int> vis_r(n, 0);
+  for (int i = 0; i < n; i++) {
+    slk.resize(n, 1e9);
+    vis_l.resize(n, 0);
+    vis_r.resize(n, 0);
+
+    while (!SparseHavePathKM(i, n, edges, slk, vis_l, vis_r, mark_l, mark_r, pas)) {
+      double tmp = 1e9;
+      for (int j = 0; j < n; j++) {
+        if (!vis_r[j])
+          tmp = std::min(tmp, slk[j]);
+      }
+      for (int j = 0; j < n; j++) {
+        if (vis_l[j])
+          mark_l[j] += tmp;
+      }
+      for (int j = 0; j < n; j++) {
+        if (vis_r[j])
+          mark_r[j] -= tmp;
+      }
+      for (int j = 0; j < n; j++) {
+        slk[j] = 1e9;
+        vis_r[j] = 0;
+      }
+    }
+  }
+}
+
 }
--- Algonoke/TestAll.cpp
+++ Algonoke/TestAll.cpp
@@ -1,23 +1,55 @@
 #include "Algonoke.h"
 #include <iostream>
+#include <string>
+
+void Check(bool statement, std::string err_info) {
+  if (!statement) {
+    std::cout << err_info << std::endl;
+    exit(0);
+  }
+}
 
 void TestKM() {
   using namespace Algo;
-  std::cout << "TestKM" << std::endl;
-  int n = 4;
-  double *cost = new double[n * n];
-  for (int i = 0; i < n * n; i++) {
-    cost[i] = 0.0;
+  {
+    std::cout << "TestKM" << std::endl;
+    int n = 4;
+    double *cost = new double[n * n];
+    for (int i = 0; i < n * n; i++) {
+      cost[i] = 0.0;
+    }
+    for (int i = 0; i < n; i++) {
+      cost[i * n + i] = -1.0;
+    }
+    std::vector<int> pas;
+    MatchingKM(n, cost, pas);
+    for (int i = 0; i < n; i++)
+      std::cout << pas[i] << " ";
+    std::cout << std::endl;
+    delete[](cost);
   }
-  for (int i = 0; i < n; i++) {
-    cost[i * n + i] = -1.0;
+  // Sparse.
+  {
+    std::cout << "--------------------------------------" << std::endl;
+    int n = 4;
+    auto edges = new std::vector<std::pair<int, double> >[n];
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (i == j) {
+          edges[i].emplace_back(j, 0.0);
+        }
+        else {
+          edges[i].emplace_back(j, 1.0);
+        }
+      }
+    }
+    std::vector<int> pas;
+    SparseMatchingKM(n, edges, pas);
+    for (int i = 0; i < n; i++)
+      std::cout << pas[i] << " ";
+    std::cout << std::endl;
+    delete[](edges);
   }
-  std::vector<int> pas;
-  MatchingKM(n, cost, pas);
-  for (int i = 0; i < n; i++)
-    std::cout << pas[i] << " ";
-  std::cout << std::endl;
-  delete[](cost);
 }
 
 int main() {
